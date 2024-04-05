8d37f5b7c7b7651c1cd6d45b6dfd9915670d4659
JUNYEON WON <wnsdus1008@gmail.com>
2024-04-05 11:55:43 +0900

    .

--- app/src/main/java/bitcamp/myapp/App.java
+++ app/src/main/java/bitcamp/myapp/App.java
@@ -1,5 +1,7 @@
 package bitcamp.myapp;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
@@ -7,19 +9,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Properties;
+import java.util.Set;
+import java.util.Map.Entry;
+
 @SpringBootApplication
 @EnableTransactionManagement
 @PropertySource({
-    "classpath:config/ncp.properties",
-    "classpath:config/ncp-secret.properties"
+    "file:${HOME}/config/jdbc.properties",
+    "file:${HOME}/config/ncp.properties",
+    "file:${HOME}/config/ncp-secret.properties"
 })
 @Controller
 public class App {
 
+  private static Log log = LogFactory.getLog(App.class);
+
   public static void main(String[] args) throws Exception {
-    System.out.println("과제관리 시스템 서버 실행!");
+    log.info("과제관리 시스템 서버 실행!");
     SpringApplication.run(App.class, args);
+    Properties props = System.getProperties();
+    Set<Entry<Object, Object>> entrySet = props.entrySet();
+
+    for (Entry<Object, Object> entry : entrySet) {
+      System.out.printf("%s=%s\n", entry.getKey(), entry.getValue());
+
+    }
   }
+
+
 
   @GetMapping("/home")
   public void home() {
@@ -28,4 +46,5 @@ public class App {
   @GetMapping("/about")
   public void about() {
   }
+
 }
